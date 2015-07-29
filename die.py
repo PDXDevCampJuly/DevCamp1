4dd5039c9c67f31b7b7c51c54194c954fc431ecb
SummerLyn <s.lynbryant@gmail.com>
2015-07-29 16:08:28 -0700

    Revert "Revert "Decoding - need beefy code""
    This reverts commit e954373c92950bd934f1cdfa6c3895787649ff69.

---
+++ die.py
@@ -0,0 +1,34 @@
+# This is a dice faking module
+from random import randint
+
+def roll(max):
+    r = randint(1, max)
+    print(r)
+    return r
+
+def roll_a_bunch(max, numOfDice=4):
+    rolls = []
+    for i in range(numOfDice):
+        rolls.append(roll(max))
+
+    return rolls
+
+def roll_distro(max, numOfDice=4):
+    #roll a bunch of dice
+    rolls = roll_a_bunch(max, numOfDice)
+
+    distribution = {}
+    #^^dictionary^^
+
+    #count what's rolled
+    for each in rolls:
+        currentCount = distribution.get(each, 0)
+        print(" Current count of ", each, ":",currentCount)
+        currentCount += 1
+        distribution[each] = currentCount
+
+    output = ""
+    for roll in distribution:
+        output += " Number " + str(roll) + " was rolled " + str(distribution[roll])
+
+    print(output)
