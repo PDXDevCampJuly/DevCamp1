1bf7c06434389682904b7f61915f66f4f94fba66
SummerLyn <s.lynbryant@gmail.com>
2015-09-16 10:37:23 -0700

    Created new for psql database practice
    Created new for psql database practice

---
+++ django/poll_site/polls/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Question
+from .models import Choice
+
+# Register your models here.
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = [ 'pub_date', 'question_text']
+    list_display = ('question_text', 'pub_date')
+    list_filter = ['pub_date', 'question_text']
+
+admin.site.register(Question, QuestionAdmin)
+
+
+
+admin.site.register(Choice)
+
